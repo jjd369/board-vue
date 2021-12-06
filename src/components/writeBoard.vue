feb8188aa00ad203d7a5daa99f037efd6e31ee18
jjd369 <ghana369@gmail.com>
2021-12-07 02:04:43 +0900

    file download api 및 css 추가

--- src/components/writeBoard.vue
+++ src/components/writeBoard.vue
@@ -12,7 +12,7 @@
       </div>
       <div class="inputWrap">
         <p>첨부 파일</p>
-        <el-upload
+        <!-- <el-upload
           :limit="1"
           :on-change="uploadFile"
           class="upload-demo"
@@ -27,10 +27,40 @@
           <div class="el-upload__tip" slot="tip"
             >jpg/png files with a size less than 500kb</div
           >
-        </el-upload>
+        </el-upload> -->
+        <el-button
+          native-type="button"
+          round
+          icon="el-icon-document-add"
+          size="mini"
+          @click="handleFileUpload"
+        ></el-button>
+        <input
+          type="file"
+          id="userImage"
+          ref="file"
+          @change="previewImage"
+          multiple
+        />
+        <transition name="slide-fade">
+          <ul v-if="files.length" class="fileListWrap">
+            <li
+              class="fileList"
+              v-for="(file, index) in this.files"
+              :key="index"
+            >
+              <span @click="deleteFileList(index)"
+                ><i class="el-icon-circle-close"></i></span
+              >{{ file.name }}
+            </li>
+          </ul>
+        </transition>
       </div>
       <div>
-        <el-button @click="$router.push({ name: 'boardList' })">목록</el-button>
+        <el-button
+          @click="$router.push({ name: 'boardList', query: { page: 1 } })"
+          >목록</el-button
+        >
         <el-button @click="writeBoard()">삽입</el-button>
       </div>
     </form>
@@ -46,22 +76,25 @@ export default {
     return {
       title: '',
       content: '',
-      file: null,
+      files: [],
     }
   },
   computed: {
     ...mapState('auth', ['current_user']),
+    // c_file_name() {
+    //   if (!this.file) return ''
+    //   return this.fil
+    // },
   },
   methods: {
     async writeBoard() {
       this.$store.dispatch('common/getLoading', true)
       const formData = new FormData()
       // body에 넣을 formData 설정
-      if (this.file) {
-        formData.append('attachment', this.file.raw, this.file.name)
-      }
-      formData.append('name', this.current_user.name)
-      formData.append('email', this.current_user.email)
+      this.files.forEach((file) => {
+        formData.append('attachment', file)
+        console.log(file)
+      })
       formData.append('title', this.title)
       formData.append('content', this.content)
 
@@ -85,18 +118,35 @@ export default {
           this.$store.dispatch('common/getLoading', false)
         })
     },
-    uploadFile(file) {
-      this.file = file
+    handleFileUpload() {
+      this.$refs.file.click()
     },
-    handleExceed() {
-      this.$message.warning({
-        type: 'warning',
-        message: '첨부파일은 1개만 가능합니다.',
-        showClose: 'true',
-      })
+    previewImage(e) {
+      if (e.target.files.length > 3) {
+        return this.$message({
+          type: 'info',
+          message: '첨부 파일 최대 개수는 3개 입니다.',
+          showClose: true,
+        })
+      }
+      this.files = [...e.target.files]
+    },
+    deleteFileList(index) {
+      this.files.splice(index, 1)
     },
   },
 }
 </script>
 
-<style></style>
+<style lang="scss">
+.fileListWrap {
+  margin-top: 10px;
+  .fileList {
+    i {
+      margin-right: 5px;
+      cursor: pointer;
+    }
+    padding: 5px;
+  }
+}
+</style>
